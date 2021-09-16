3eea4af5f73cf5a9bceb8115e64d77b7624e28fc
Andrey Maksimov <maksimov@mandarin.solutions>
2021-09-16 16:26:16 +0700

    this may first commit

--- src/main/java/FirstProject.java
+++ src/main/java/FirstProject.java
@@ -1,8 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstProject {
 
 
     public static void main(String[] args) {
-        System.out.println("sdfsdf () ()");
+        int i =0;
+        while (i <= 30) {
+            System.out.println("sdfsdf () ()" + i);
+            i= i+1;
+        }
+
+        List<String> list  = new ArrayList<>();
+
     }
 }
