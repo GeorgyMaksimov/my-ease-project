468cd48d94c7a2167b4ed405acaf0b225d07564a
Gidzhen <gosha131298@gmail.ru>
2021-09-18 17:26:33 +0600

    Возраст

---
+++ src/main/java/secondprject.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class secondprject {
+
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = reader.nextInt();
+        int y = 10;
+        int z = x % y;
+        if (x > 99) {
+            System.out.print("Error");
+        }
+        else {
+            if (x < 1) {
+                System.out.print("Error");
+            }
+            else {
+                switch (z) {
+                    case (0):
+                        System.out.print(x + "Лет");
+                        break;
+                    case (1):
+                        System.out.print(x + "Год");
+                        break;
+                    case (2):
+                        System.out.print(x + "Года");
+                        break;
+                    case (3):
+                        System.out.print(x + "Года");
+                        break;
+                    case (4):
+                        System.out.print(x + "Года");
+                        break;
+                    case (5):
+                        System.out.print(x + "Лет");
+                        break;
+                    case (6):
+                        System.out.print(x + "Лет");
+                        break;
+                    case (7):
+                        System.out.print(x + "Лет");
+                        break;
+                    case (8):
+                        System.out.print(x + "Лет");
+                        break;
+                    case (9):
+                        System.out.print(x + "Лет");
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
