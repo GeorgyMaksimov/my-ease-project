a182342540f3bdb36434ccc9ee67dc1c9ac725a4
Gidzhen <gosha131298@gmail.ru>
2021-09-18 14:44:08 +0600

    Цикл

--- src/main/java/FirstProject.java
+++ src/main/java/FirstProject.java
@@ -3,11 +3,11 @@ public class FirstProject {
 
     public static void main(String[] args) {
         for (int x =0;  x < 21; x=x+1) {
-            System.out.println(x);
-            for (int y=0;y < 21; y=y+1) {
-                System.out.print(y);
+            for (int y=1;y < 21; y=y+1) {
+                System.out.print(x);
+                System.out.println(y);
             }
-
         }
+
     }
 }
