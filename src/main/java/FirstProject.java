1269523dbaed53bd52b4beed8857618b32e560d7
Gidzhen <gosha131298@gmail.ru>
2021-09-18 14:48:08 +0600

    Цикл

--- src/main/java/FirstProject.java
+++ src/main/java/FirstProject.java
@@ -3,7 +3,7 @@ public class FirstProject {
 
 
     public static void main(String[] args) {
-        for (int x =0;  x < 21; x=x+1) {
+        for (int x =1;  x < 6; x=x+1) {
             System.out.print(x);
             }
         }
