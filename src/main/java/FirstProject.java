fe29af1afee92305fc0fdcfe30e6006d6a10c24b
Gidzhen <gosha131298@gmail.ru>
2021-09-18 14:30:06 +0600

    Цикл1

--- src/main/java/FirstProject.java
+++ src/main/java/FirstProject.java
@@ -1,14 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class FirstProject {
 
 
     public static void main(String[] args) {
-        for (int x =0;  x < 21; x=+1) {
+        for (int x =0;  x < 21; x=x+1) {
             System.out.println(x);
-            for (int x;x<21; x=+1) {
-                System.out.print(x);
+            for (int y=0;y < 21; y=y+1) {
+                System.out.print(y);
             }
 
         }
