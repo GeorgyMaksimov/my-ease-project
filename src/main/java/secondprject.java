a830f2c9a7f8d0ce250bf1ccd8b543ffad5450a8
Kit <pinigins55@gmail.com>
2021-09-20 22:33:07 +0300

    Замечаения

--- src/main/java/secondprject.java
+++ src/main/java/secondprject.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class secondprject {
 
     static Scanner reader = new Scanner(System.in);
-    public static int s (int x){
-        int x = reader.nextInt();
+    public static int s (int x){//у тебя тут x
+        int x = reader.nextInt();//и тут x
         int y = 10;
         int z =x%y;
         return z;
     }
 
+    //Где вызов твоего метода s? Вообще, из за того что ты назвал метод s непонятно что это метод, методы принято называть глаголом
+    //типа getReminder
     public static void main(String[] args) {
-        int x = reader.nextInt();
-        return z;
+        int x = reader.nextInt();//ты считывание вызываешь в двух местах, зачем? Задание было передать в функцию значение
+        return z; //Зачем этот ретурн, что он делает?
         if (x > 99) {
             System.out.print("Error");
         }
