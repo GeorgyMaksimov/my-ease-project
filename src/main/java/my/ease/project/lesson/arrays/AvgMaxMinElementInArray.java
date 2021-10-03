38bcaa56a0f1bae9f0d2433f40367325f3e8f282
Andrey Maksimov <maksimov@mandarin.solutions>
2021-10-03 17:04:57 +0700

    add new task

---
+++ src/main/java/my/ease/project/lesson/arrays/AvgMaxMinElementInArray.java
@@ -0,0 +1,18 @@
+package my.ease.project.lesson.arrays;
+
+/**
+ * @author Maksimov
+ */
+public class AvgMaxMinElementInArray {
+    /**
+     * Заполните массив случайным числами и выведите максимальное, минимальное и среднее значение.
+     * <p>
+     * Для генерации случайного числа используйте метод Math.random(), который возвращает значение в промежутке [0, 1].
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/my/ease/project/lesson/arrays/PrimeNumbers.java
@@ -0,0 +1,7 @@
+package my.ease.project.lesson.arrays;
+
+/**
+ * @author Maksimov
+ */
+public class PrimeNumbers {
+}
