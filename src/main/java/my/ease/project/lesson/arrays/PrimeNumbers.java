dedae4165ebd2671dfb1d701259501418775b574
Andrey Maksimov <maksimov@mandarin.solutions>
2021-10-03 17:20:00 +0700

    add new description

--- src/main/java/my/ease/project/lesson/arrays/PrimeNumbers.java
+++ src/main/java/my/ease/project/lesson/arrays/PrimeNumbers.java
@@ -4,4 +4,12 @@ package my.ease.project.lesson.arrays;
  * @author Maksimov
  */
 public class PrimeNumbers {
+
+    /**
+     * Напишите программу, которая выводит на консоль простые числа в промежутке от [2, 100].
+     * Используйте для решения этой задачи оператор "%" (остаток от деления) и циклы.
+     */
+    public static void main(String[] args) {
+
+    }
 }
