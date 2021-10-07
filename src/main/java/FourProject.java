9b01aac9645aac2f20a6a89e76617c905a96546c
Gidzhen <gosha131298@gmail.ru>
2021-10-07 14:32:52 +0300

    FourProject.java

--- src/main/java/FourProject.java
+++ src/main/java/FourProject.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
+class FourProject {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+        int z = 0;
+        int i = 0;
+        for(i;i<100; i++)
+        {
+            z = x + y;
+            x = z + y;
+            y = z + x;
+        }
 
-public class FourProject {
 
-    int x = 1;
-    int y = 1;
-    int z = 0;
-    int i=0;
-    for (i;i=100;i+=1){
-        z = x + y;
-        x = z + y;
-        y = z + x;
     }
-
-
 }
