5136ab8c5a0284fe399c3e098180555e2d7e2b11
Gidzhen <gosha131298@gmail.ru>
2021-10-07 14:26:07 +0300

    FourProject.java

---
+++ src/main/java/FourProject.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FourProject {
+
+    int x = 1;
+    int y = 1;
+    int z = 0;
+    int i=0;
+    for (i;i=100;i+=1){
+        z = x + y;
+        x = z + y;
+        y = z + x;
+    }
+
+
+}
