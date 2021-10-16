e47388b1dc3e6bea19ecb6d8bac09eb8dc86bc81
Gidzhen <gosha131298@gmail.ru>
2021-10-16 15:23:08 +0300

    second version PrimeNumbers

--- src/main/java/my/ease/project/lesson/arrays/PrimeNumbers.java
+++ src/main/java/my/ease/project/lesson/arrays/PrimeNumbers.java
@@ -1,6 +1,6 @@
 package my.ease.project.lesson.arrays;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @author Maksimov
@@ -10,23 +10,52 @@ public class PrimeNumbers {
     /**
      * Напишите программу, которая выводит на консоль простые числа в промежутке от [2, 100].
      * Используйте для решения этой задачи оператор "%" (остаток от деления) и циклы.
-     * @param args main arg
      *
+     * @param args main arg
      */
     public static void main(String[] args) {
-        int y = 0;
-        int i = 0;
-        int z = 0;
-        for (int x=0;x<100;x=x+1){
-            y++;
-            i= y%2;
-            z = y%3;
-            if (i == 0 && z == 0){
-                System.out.print(""+y);
-            } else (){
-                System.out.print(""+y);
+        printSimpleNumber1();
+        System.out.println("hello");
+        printSimpleNumber2();
+    }
+
+    private static void printSimpleNumber2() {
+        for (int number=2;number<201;number++){
+           int remainder=0;
+            for (int divider=1;divider<=number;divider++){
+                if (number%divider==0) {
+                    remainder++;
+                }
             }
+            if (remainder==2){
+                System.out.print(" "+number);
+            }
+        }
+    }
+
+    private static void printSimpleNumber1() {
+        Set<Integer> dividers =new HashSet<>();
+        dividers.add(1);
+       //dividers.add(2);
+        // dividers.add(3);
+        for (int x = 2; x < 201; x++) {
+            int remainer=0;
+            int lastDivider=0;
+            for (Integer divider:dividers) {
+                if (x % divider == 0) {
+                    remainer++;
+                    lastDivider=divider;
+                }
+            }
+            if (lastDivider<x){
+                remainer++;
+            }
+            if (remainer==2){
+                System.out.print(x+" ");
+                dividers.add(x);
             }
         }
     }
 }
+
+
