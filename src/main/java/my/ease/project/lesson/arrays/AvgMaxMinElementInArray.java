f5b299ece8413859acb534fdaedb3447aa88428e
Gidzhen <gosha131298@gmail.ru>
2021-11-01 09:27:50 +0300

    first version

--- src/main/java/my/ease/project/lesson/arrays/AvgMaxMinElementInArray.java
+++ src/main/java/my/ease/project/lesson/arrays/AvgMaxMinElementInArray.java
@@ -1,9 +1,13 @@
 package my.ease.project.lesson.arrays;
 
+import myArray.lenght;
+
 /**
  * @author Maksimov
  */
 public class AvgMaxMinElementInArray {
+    private static Object lenght;
+
     /**
      * Заполните массив случайным числами и выведите максимальное, минимальное и среднее значение.
      * <p>
@@ -12,7 +16,19 @@ public class AvgMaxMinElementInArray {
      * @param args main arg
      */
     public static void main(String[] args) {
-
+        double array = Math.random(0, 1);
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int minId = i;
+            for (int j = i+1; j < array.lengtht; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minId = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = min;
+            array[minId] = temp;
+        }
     }
-
-}
+    }
