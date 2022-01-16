b0f5b86cf1c6322bafbd5826a65f45b87dcb3481
Gidzhen <gosha131298@gmail.ru>
2022-01-16 19:51:37 +0300

    FirstVersion ArrayNew

---
+++ src/main/java/my/ease/project/lesson/arrays/ArrayNew.java
@@ -0,0 +1,8 @@
+package my.ease.project.lesson.arrays;
+
+public class ArrayNew {
+    public static void main (String[] args){
+        int[] array = Math.round(100 * Math.random());
+        System.out.print("Массив: "+array);
+    }
+}
