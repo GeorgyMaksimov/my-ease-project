a9b3803340947a9f75d96b0f788791b3e8db1ae5
Gidzhen <gosha131298@gmail.ru>
2022-01-17 17:50:20 +0300

    ThreeVersion ArrayNew

--- src/main/java/my/ease/project/lesson/arrays/ArrayNew.java
+++ src/main/java/my/ease/project/lesson/arrays/ArrayNew.java
@@ -4,8 +4,13 @@ import static java.lang.Math.round;
 
 public class ArrayNew {
     public static void main(String[] args) {
-        int[] array = new int[50];
-        array = Math.round(100 * Math.random());
-        System.out.print("Массив: " + array);
+        int[] Array = new int[50];
+        System.out.print("Массив");
+        for (int i = 0; i < Array.length ; i++) {
+            long ArrayElement = round(100 * Math.random());
+            Array[i] = ArrayElement;
+            System.out.print(" " + Array[i]);
+        }
+
     }
 }
