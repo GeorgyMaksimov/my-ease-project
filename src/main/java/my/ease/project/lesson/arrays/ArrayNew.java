8431fbedb787bec255caea3de4cbb5f10e6c252b
Andrey Maksimov <maksimov@mandarin.solutions>
2022-01-17 21:58:20 +0700

    fix after CR

--- src/main/java/my/ease/project/lesson/arrays/ArrayNew.java
+++ src/main/java/my/ease/project/lesson/arrays/ArrayNew.java
@@ -4,12 +4,12 @@ import static java.lang.Math.round;
 
 public class ArrayNew {
     public static void main(String[] args) {
-        int[] Array = new int[50];
+        int[] array = new int[50];
         System.out.print("Массив");
-        for (int i = 0; i < Array.length ; i++) {
-            long ArrayElement = round(100 * Math.random());
-            Array[i] = ArrayElement;
-            System.out.print(" " + Array[i]);
+        for (int i = 0; i < array.length; i++) {
+            long arrayElement = round(100 * Math.random());
+            array[i] = (int) arrayElement;
+            System.out.print(" " + array[i]);
         }
 
     }
