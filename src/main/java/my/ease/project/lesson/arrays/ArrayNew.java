9c751daddfc863c658a24e2e0e539c2665faa5d6
Gidzhen <gosha131298@gmail.ru>
2022-01-17 17:14:53 +0300

    SecondVersion ArrayNew

--- src/main/java/my/ease/project/lesson/arrays/ArrayNew.java
+++ src/main/java/my/ease/project/lesson/arrays/ArrayNew.java
@@ -1,8 +1,12 @@
 package my.ease.project.lesson.arrays;
 
+import static java.lang.Math.round;
+import static java.lang.Math.random;
+
 public class ArrayNew {
     public static void main (String[] args){
-        int[] array = Math.round(100 * Math.random());
+        int [] array = new int [50];
+        array = int Math.round (100 * Math.random());
         System.out.print("Массив: "+array);
     }
 }
