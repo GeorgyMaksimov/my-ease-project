0d9dd6b65648c4eb14e7e9b15bc1fe2cee75f756
Gidzhen <gosha131298@gmail.ru>
2022-01-18 16:15:55 +0300

    fourVersion ArrayNew

--- src/main/java/my/ease/project/lesson/arrays/ArrayNew.java
+++ src/main/java/my/ease/project/lesson/arrays/ArrayNew.java
@@ -4,13 +4,20 @@ import static java.lang.Math.round;
 
 public class ArrayNew {
     public static void main(String[] args) {
-        int[] Array = new int[50];
-        System.out.print("Массив");
-        for (int i = 0; i < Array.length ; i++) {
-            long ArrayElement = round(100 * Math.random());
-            Array[i] = ArrayElement;
-            System.out.print(" " + Array[i]);
+        int[] array = new int[50];
+        System.out.print("Массив 1");
+        int arrayElement;
+        for (arrayElement = 0; arrayElement < array.length; arrayElement++) {
+            long i = round(100 * Math.random());
+            array[arrayElement] = (int) i;
+            System.out.print(" " + array[arrayElement]);
+            int RemainderOfTheDivision = arrayElement % 3;
+            if (RemainderOfTheDivision == 0) {
+                array[arrayElement] = 0;
+            }
         }
-
+        System.out.println();
+        System.out.print("Массив 2: "+array);
+        }
     }
-}
+
