678c8b6f21ecf11ff63e6bb40f5c8834f3d00df3
Gidzhen <gosha131298@gmail.ru>
2022-01-25 12:53:12 +0300

    finish version

--- src/main/java/my/ease/project/lesson/arrays/NewArray.java
+++ src/main/java/my/ease/project/lesson/arrays/NewArray.java
@@ -1,19 +1,19 @@
 package my.ease.project.lesson.arrays;
 
+import static java.lang.Math.multiplyFull;
 import static java.lang.Math.round;
 
 public class NewArray {
-    // где условия задачи?
+    /**
+     1. Создайте массив из 50 случайных целых чисел
+     2. Выведите массив на экран в строку
+     3. Замените каждый элемент с  индексом кратный 3  на ноль
+     4. Снова выведете массив на экран на отдельной строке
+           */
+    
     public static void main(String[] args) {
-        int[] array = new int[50];
-        //зачем тут обявлен arrayElement?
-        //используй ArrayHelper для запрления зачем дублировать код?
-        int arrayElement;
-        for (arrayElement = 0; arrayElement < array.length; arrayElement++) {
-            long i = round(100 * Math.random());
-            array[arrayElement] = (int) i;
-            System.out.print(" " + array[arrayElement]);
-        }
+        int[] array=new int[50];
+        ArrayHelper.fill(array);
         System.out.println();
 
         for (int index = 0; index < array.length; index++) {
